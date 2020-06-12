6fa44a464962f5c2795b035f4f7d063dda6bb71d
davecarroll <llorrac.evad@gmail.com>
2020-06-12 13:37:08 -0700

    testing

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var request  = require('request');
 var ss       = require('socket.io-stream');
 var Readable = require('stream').Readable;
 var fs = require('fs');
-const io     = require('socket.io')(http);    //import socket.io and socket.io-streams
+const io = require('socket.io')(http);    //import socket.io and socket.io-streams
 var bodyParser = require('body-parser')
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 const localPort   = 3000;                          //local host port
@@ -16,78 +16,33 @@ app.use(express.static('scripts'));           //allow Node.js access scripts dir
 app.use(express.static(__dirname));
 var dgram = require('dgram');
 var server = dgram.createSocket('udp4');
-var disconnected = false;
 
 var gazeLog = fs.createWriteStream("./userGazeLog.txt");
 
-const encodeGetParams = p =>
-  Object.entries(p).map(kv => kv.map(encodeURIComponent).join("=")).join("&");
 
-server.on('listening', function () {
-    var address = server.address();
-    console.log('UDP Server listening on ' + address.address + ":" + address.port);
-});
 
-io.of('/data').on('connection', socket => {//broadcast for socket connection
-  console.log("connection to client complete.");//console feedback for user
-  if(disconnected) {
-    server = dgram.createSocket('udp4');
-  }
-  server.on('message', function (message, remote) {
-    var toSend = parseMessage(message);
-    var s = new Readable();
-    s._read = function() {};
-    var stream = ss.createStream();
-    //push the gaze data to the readable stream
-    s.push(toSend);
-    s.pipe(stream);
-    ss(socket).emit('gaze', stream); //broadcast the stream
-  });
-
-  server.bind(PORT, HOST);
-  //log a disconnect from the socket
-  socket.on('disconnect', () => {
-    disconnected = true;
-    console.log("disconnected, closing udp server");
-    server.close();
-  });
-
-  socket.on('gazeLog', function(data) {
-    //console.log(data);
-    var dataString = JSON.stringify(data);
-    dataString = dataString + "\n";
-    gazeLog.write(dataString);
+const users = io.of("/users").on("connection", (socket) => {
+  socket.on("userID", (msg) => {
+    console.log(msg);
   });
 });
 
-io.of('/gazeLog').on('connection', socket => {
-  console.log("gaze log connection complete");
+const mouse = io.of("/mouse").on("connection", (socket) => {
+  socket.on("mouse move", (msg) => {
+    console.log(msg);
+  });
+});
 
-})
-
-var previousTimestamp = 0;
-
-function parseMessage(message){
-  var messageObj = JSON.parse(message);
-  if(messageObj.timestamp>previousTimestamp){
-    previousTimestamp = messageObj.timestamp;
-    return messageObj.x + "," + messageObj.y;
-  } else {
-    return null;
-  }
-}
-
-//send a request with response result with express
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, './index.html'));
-})
+});
 
 app.post('/log', function(req, res) {
  console.log(req.body);
- if(req.body.hasOwnProperty('line')) {
+ if(req.body.hasOwnProperty('type')) {
    gazeLog.write(JSON.stringify(req.body));
    gazeLog.write("\n");
-   console.log("logged gaze");
+   console.log("logged mouse move");
  }
 });
 
---
+++ scripts/mouseMove.js
@@ -0,0 +1,43 @@
+const mouse = io.connect("http://localhost:3000/mouse");
+const users = io.connect("http://localhost:3000/users");
+
+const throttle = 120;
+var start = (new Date).getTime();
+
+users.on("connect", () => {
+  users.emit("userID", "user: " + window.userId + " has connected");
+
+  document.addEventListener("mousemove", handleMouseMove);
+});
+
+users.on("disconnect", () => {
+  users.emit("userIDDisconnect", "user: " + window.userId + " has disconnected");
+});
+
+
+function handleMouseMove(event) {
+  event = event || window.event;
+
+  let now = (new Date).getTime();
+
+  if (now - start >= throttle) {
+    logMouseMovement(event);
+  } else {
+    return;
+  }
+  start = now;
+
+};
+
+var logMouseMovement = function (mouseEvent) {
+  let currentTime = (new Date).getTime();
+
+  let mouseMove = {};
+  mouseMove.user = window.userId;
+  mouseMove.x = mouseEvent.clientX;
+  mouseMove.y = mouseEvent.clientY;
+  mouseMove.epoch = currentTime + 47000;
+
+  mouse.emit("mouse move", JSON.stringify(mouseMove));
+};
+
