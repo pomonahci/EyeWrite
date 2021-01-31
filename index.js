6001605528730250fcb3005069e319e605f1db26
chanhakim <chanhakim17@gmail.com>
2021-01-30 18:36:04 -0800

    madded css for toggle switches (will replace on/off buttons)

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-var path = require('path');               //documentation: https://nodejs.org/api/path.html
-var express = require('express');            //connect with express
+var path = require('path');                   // documentation: https://nodejs.org/api/path.html
+var express = require('express');             // connect with express
 var app = express();
-var http = require('http').Server(app);
+var https = require('https');
 var bodyParser = require('body-parser')
-app.use(bodyParser.json());       // to support JSON-encoded bodies
-const localPort = 3000;                          //local host port
-app.use(express.static('scripts'));           //allow Node.js access scripts directory
+app.use(bodyParser.json());                   // to support JSON-encoded bodies
+const localPort = 3000;                       // local host port
+app.use(express.static('scripts'));           // allow Node.js access scripts directory
 app.use(express.static(__dirname));
+var fs = require("fs");
 
+const options = {
+  key: fs.readFileSync("./https/server.key"),
+  cert: fs.readFileSync("./https/server.crt")
+};
 
 app.get('/', function (req, res) {
   console.log(__dirname);
@@ -15,6 +20,6 @@ app.get('/', function (req, res) {
 });
 
 //run server
-http.listen(localPort, function () {
+https.createServer(options, app).listen(localPort, function () {
   console.log("server running on port: " + localPort);
 });
