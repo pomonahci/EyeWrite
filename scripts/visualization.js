bb58ad2ac783453d040a3b5e5b7eb1c4d5f5f036
chanhakim <chanhakim17@gmail.com>
2021-02-22 22:25:50 -0800

    fixed math issues

--- scripts/visualization.js
+++ scripts/visualization.js
@@ -464,7 +464,7 @@ var mouseVis = function () {
     if (index != -1) {
       let numPad = Math.floor(slider.value / 2);
       if (slider.value % 2 == 0) {
-        if (ch <= Math.floor(token.end - token.start / 2)) {
+        if (ch <= Math.floor((token.end - token.start) / 2)) {
           return { left: line - numPad, right: line + numPad };
         } else {
           return { left: line - numPad + 1, right: line + numPad + 1 };
