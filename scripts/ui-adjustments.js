c3c378b87170dbe298f8cf8a6c08e8b7b1832087
chanhakim <chanhakim17@gmail.com>
2021-05-04 17:42:35 -0700

    refactor: move distinct color function from eyewrite.html to ui-adjustments.js

--- scripts/ui-adjustments.js
+++ scripts/ui-adjustments.js
@@ -1,3 +1,10 @@
+/**
+ * ui-adjustments.js handles some UI customization
+ * 
+ * Name: Dave
+ * Date: n/a
+ */
+
 var UIAdjustments = function () {
   firepadUserList.firebaseOff_(firebaseRef.child('users'), 'child_changed', firepadUserList['firebaseCallbacks_'][4]['callback']);
 
@@ -16,7 +23,7 @@ var UIAdjustments = function () {
   var colorEditPencilIcon = document.createElement('img');
   colorEditPencilIcon.id = "color-edit-pencil-icon";
   colorEditPencilIcon.src = './graphics/pencil.png';
-  
+
   var el = document.createElement('button');
   el.className = 'colorpicker-button';
   el.appendChild(colorEditPencilIcon);
@@ -80,6 +87,75 @@ var UIAdjustments = function () {
 
   var controlContainer = document.getElementById('controlContainer');
   controlContainer.style.top = (userlistBox.offsetTop + userlistBox.offsetHeight + 10) + "px";
+
+  firebaseRef.child('users').once("value", function (data) {
+    // do some stuff once
+    var existingColors = []
+    if (data.key != userId) {
+      for (let [uID, val] of Object.entries(data.val())) {
+        if (uID != userId) {
+          existingColors.push(chroma(val['color']).hcl());
+          // if (window.debug) console.log(chroma(val['color']).hcl());
+        }
+      };
+    }
+    if (existingColors.length > 0) {
+      firepad.firebaseAdapter_.setColor('#ffffff');
+    }
+    console.log(existingColors, 'Now generate a new color for current user!');
+
+    var newUserColor = select_new_color(existingColors, firepad.firebaseAdapter_.color_);
+    console.log(newUserColor, chroma(newUserColor).hcl());
+    firepad.firebaseAdapter_.setColor(newUserColor);
+
+  });
+
+  function select_new_color(existingColors, currentColor) {
+    var numColors = existingColors.length;
+    if (numColors == 0) {
+      return currentColor;
+    }
+
+    existingColors.sort(function (a, b) {
+      if (a[0] == b[0]) {
+        return 0;
+      } else {
+        return a[0] < b[0] ? -1 : 1;
+      }
+    });
+
+    // console.log(existingColors);
+
+    var l = Math.max(Math.min(Math.random() * 100.0, 90.0), 30.0);
+    var c = Math.max(Math.min(Math.random() * 100.0, 75.0), 25.0);
+
+    var hue_min = 0;
+    var hue_max = 0;
+
+    for (var i = 1; i < numColors; i++) {
+      var prev_hue = existingColors[i - 1][0];
+      var curr_hue = existingColors[i][0];
+
+      if (curr_hue - prev_hue > hue_max - hue_min) {
+        hue_min = prev_hue;
+        hue_max = curr_hue;
+      }
+      console.log(hue_min, hue_max);
+    }
+
+    var lst_hue = existingColors[numColors - 1][0];
+    var fst_hue = existingColors[0][0] + 360;
+
+    if (fst_hue - lst_hue > hue_max - hue_min) {
+      hue_min = lst_hue;
+      hue_max = fst_hue;
+    }
+
+    var h = (hue_min + (hue_max - hue_min) / 2) % 360;
+    var newColor = chroma(h, c, l, 'hcl').hex();
+
+    return newColor;
+  }
 
   return {
     userlistBoxOffsetWidth: userlistBox.offsetWidth
