97fffb299ff6f2de818b1aead861e3837dd2a7c8
nmarsano <nickmarsano@gmail.com>
2021-06-09 11:59:27 -0700

    media testing

--- scripts/media-call.js
+++ scripts/media-call.js
@@ -235,9 +235,9 @@ var mediaCall = function () {
 		if (!videoElts[stream.id]) {
 			var video = document.createElement("video");
 			video.setAttribute("width","175px");
-			video.setAttribute("muted","true");
 			video.setAttribute("style","box-shadow: 0 0 0 5pt "+userColors[stream.id]);//need to use streamid to query the actual userif (user if takes local id)
 			video.autoplay = true;
+			video.setAttribute("muted","true");
 			video.load();
 			video.addEventListener("load", function () {
 				video.play();
