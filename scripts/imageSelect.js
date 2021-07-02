4fe49c4a47d47dc1960de0956c4c44a12b2a3521
nmarsano <nickmarsano@gmail.com>
2021-07-02 13:02:03 -0700

    error case

--- scripts/imageSelect.js
+++ scripts/imageSelect.js
@@ -32,6 +32,7 @@ function getImage() {
     var URL = window.location.href;
     imageLabel = URL.search("img");
     imageLabel = URL.substring(imageLabel + 4, imageLabel + 5);
+    if(ImageLabel==-1)return;
     numPpl = URL.search('par');
     if (numPpl == -1) {
         numPpl = 2;
@@ -45,6 +46,7 @@ function getImage() {
 }
 
 function getTarget() {
+    if(!bounding)return;
     var keys = Object.keys(bounding);
     target = bounding[keys[task]];
     console.log(keys[task]);
