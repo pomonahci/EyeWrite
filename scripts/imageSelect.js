52b74d24fc718eba34f56d41700e2cc120baa068
nmarsano <nickmarsano@gmail.com>
2021-07-13 16:34:29 -0700

    misclick listener

--- scripts/imageSelect.js
+++ scripts/imageSelect.js
@@ -56,8 +56,8 @@ function getImage() {
 
 
 function getTarget() {
-    firebaseRef.child('tasks').child(task).child('targetClicked').set([]);
-    // firebaseRef.child('tasks').child(task).child('incorrectClicks').set("");
+    firebaseRef.child('tasks').child(task).child('targetClicked').set("");
+    firebaseRef.child('tasks').child(task).child('incorrectClicks').set("");
 
     // firebaseRef.child('tasks').child(task).child('targetClicked').on('child_added', checkTaskComplete);//useless in experiments with more than 1 person
     // firebaseRef.child('tasks').child(task).child('targetClicked').on('child_changed', checkTaskComplete);
