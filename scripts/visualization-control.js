b7afb251ff32cf1acf8be615ea762d85f4674e12
Andy Han <36119601+andyqhan@users.noreply.github.com>
2021-12-09 13:54:59 -0800

    add gaze overlap in csv

--- scripts/visualization-control.js
+++ scripts/visualization-control.js
@@ -930,6 +930,12 @@ var visualizationControl = (function () {
         }
       })
       if (overlapping.length > 0) {//if overlap
+        var laps = "";
+        for (const item of overlapping) {
+          laps = laps + ":" + item;
+        }
+        serverContent.push(["Overlapping", laps]);
+
         if (deterministic == 2 && unique) {//color combinations for overlap
           rgbs = hColor.substring(5, hColor.length - 1).split(',');
           hColor = "rgba(";
