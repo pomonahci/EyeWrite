40c18ef15879e3f13aea94426098d8b59f83d9fe
haram082 <haramkim082@gmail.com>
2024-06-11 15:07:21 -0700

    first cocmmit

--- scripts/downloadData.js
+++ scripts/downloadData.js
@@ -104,4 +104,4 @@ function unloadingCSV() {
 
 }
 // window.addEventListener('beforeunload', unloading);
-window.addEventListener('beforeunload', unloadingCSV);
+// window.addEventListener('beforeunload', unloadingCSV);
