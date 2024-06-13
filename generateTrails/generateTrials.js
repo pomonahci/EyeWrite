05c401067b79041b88ce82c62ecb3d285856e06c
haram082 <haramkim082@gmail.com>
2024-06-13 14:54:12 -0700

    json

--- generateTrails/generateTrials.js
+++ generateTrails/generateTrials.js
@@ -3,6 +3,7 @@ const ctx = canvas.getContext('2d');
 const generateButton = document.getElementById('generateButton');
 const imageCountInput = document.getElementById('imageCount');
 const imageSizeInput = document.getElementById('imageSize');
+let O_coords = {};
 
 const canvasWidth = 700;
 const canvasHeight = 500;
@@ -11,12 +12,13 @@ const minDistance = 50;
 canvas.width = canvasWidth;
 canvas.height = canvasHeight;
 
+
 function drawLetter(letter, x, y, rotation) {
   ctx.save();
   ctx.translate(x, y);
   ctx.rotate(rotation * Math.PI / 180);
   ctx.fillStyle = 'black';
-  ctx.font = '25px Arial';
+  ctx.font = '14px Arial';
   ctx.textAlign = 'center';
   ctx.textBaseline = 'middle';
   ctx.fillText(letter, 0, 0);
@@ -25,7 +27,7 @@ function drawLetter(letter, x, y, rotation) {
   const textMetrics = ctx.measureText(letter);
   const rectWidth = textMetrics.width + 6;
   const rectHeight = parseInt(ctx.font, 10) + 6;
-  ctx.strokeRect(-rectWidth / 2, -rectHeight / 2, rectWidth, rectHeight); // Add this line to draw a rectangle around the letter
+
   ctx.restore();
 
   const topLeftX = x - rectWidth / 2;
@@ -36,28 +38,31 @@ function drawLetter(letter, x, y, rotation) {
   const bottomLeftY = y + rectHeight / 2;
   const bottomRightX = x + rectWidth / 2;
   const bottomRightY = y + rectHeight / 2;
+  // ctx.fillStyle = 'red';
+  // ctx.beginPath();
+  // ctx.arc(topLeftX, topLeftY, 3, 0, 2 * Math.PI);
+  // ctx.fill();
 
-  console.log(`Top Left: (${topLeftX}, ${topLeftY})`);
-  console.log(`Top Right: (${topRightX}, ${topRightY})`);
-  console.log(`Bottom Left: (${bottomLeftX}, ${bottomLeftY})`);
-  console.log(`Bottom Right: (${bottomRightX}, ${bottomRightY})`);
+  // ctx.beginPath();
+  // ctx.arc(topRightX, topRightY, 3, 0, 2 * Math.PI);
+  // ctx.fill();
 
-  ctx.fillStyle = 'red';
-  ctx.beginPath();
-  ctx.arc(topLeftX, topLeftY, 3, 0, 2 * Math.PI);
-  ctx.fill();
+  // ctx.beginPath();
+  // ctx.arc(bottomLeftX, bottomLeftY, 3, 0, 2 * Math.PI);
+  // ctx.fill();
 
-  ctx.beginPath();
-  ctx.arc(topRightX, topRightY, 3, 0, 2 * Math.PI);
-  ctx.fill();
+  // ctx.beginPath();
+  // ctx.arc(bottomRightX, bottomRightY, 3, 0, 2 * Math.PI);
+  // ctx.fill();
 
-  ctx.beginPath();
-  ctx.arc(bottomLeftX, bottomLeftY, 3, 0, 2 * Math.PI);
-  ctx.fill();
-
-  ctx.beginPath();
-  ctx.arc(bottomRightX, bottomRightY, 3, 0, 2 * Math.PI);
-  ctx.fill();
+  if (letter === 'O') {
+    O_coords = {
+      topLeft: { x: topLeftX, y: topLeftY },
+      topRight: { x: topRightX, y: topRightY },
+      bottomLeft: { x: bottomLeftX, y: bottomLeftY },
+      bottomRight: { x: bottomRightX, y: bottomRightY }
+    };
+  } 
 }
 
 function isOverlapping(x, y, positions) {
@@ -107,21 +112,50 @@ function downloadImage(filename) {
   link.click();
 }
 
+
+
 function generateAndDownloadImages() {
   const imageCount = parseInt(imageCountInput.value);
+  const trialsData = [];
 
   for (let i = 1; i <= imageCount; i++) {
     generateImage(true);
+
     document.body.appendChild(canvas);
     canvas.style.display = 'block';
     canvas.style.border= '1px solid black';
-    // downloadImage(`present_small_${i}.jpg`);
+    const presentData = {
+      name: `${imageSizeInput.value}_present_${i}.jpg`,
+      id: i,
+      absent: false,
+      size: parseInt(imageSizeInput.value),
+      O_coordinates: O_coords
+    };
+    trialsData.push(presentData);
+    downloadImage(`${imageSizeInput.value}_present_${i}.jpg`);
 
-    // generateImage(false);
-    // document.body.appendChild(canvas);
-    // canvas.style.display = 'block';
-    // downloadImage(`absent_small_${i}.jpg`);
+    
+    generateImage(false);
+    const absentData = {
+      name: `${imageSizeInput.value}_absent_${i}.jpg`,
+      id: i,
+      absent: true,
+      size: parseInt(imageSizeInput.value),
+      O_coordinates: O_coords
+    };
+    trialsData.push(absentData);
+    downloadImage(`${imageSizeInput.value}_absent_${i}.jpg`);
   }
+    console.log(trialsData);
+    const jsonString = JSON.stringify(trialsData);
+    const blob = new Blob([jsonString], { type: 'application/json' });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = 'trials.json';
+    link.click();
+    URL.revokeObjectURL(url);
+  
 }
 
 generateButton.addEventListener('click', generateAndDownloadImages);
